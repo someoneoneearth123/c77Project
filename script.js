fc6efff90aeac35eead84d1cb00ececa9b85c5a8
someoneoneearth123 <114456663+someoneoneearth123@users.noreply.github.com>
2022-09-26 19:29:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var firstName = "John";
+var lastName = "Doe";
+var combinedName = firstName.concat(lastName);
+
+var a = 15;
+var b = 12;
+var add = a + b;
+var subtract = a - b;
+var multiply = a * b;
+var divide = a / b;
+
+console.log(combinedName);
+console.log(`Addition: ${add}`);
+console.log(`Subtraction: ${subtract}`);
+console.log(`Multiplication: ${multiply}`);
+console.log(`Division: ${divide}`);
